899fd2c1ebdd3940ec06a272bb94a6d6755fb02b
Krzysztof-Chmielewski-Eclipse <69456141+Krzysztof-Chmielewski-Eclipse@users.noreply.github.com>
2020-09-24 11:02:31 +0200

    Add simple SpringBoot Application Add Maven wrapper Add Gradle wrapper

---
+++ src/main/java/uk/co/eclipsegroup/training/springmvc/TrainingSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package uk.co.eclipsegroup.training.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/eclipsegroup/training/springmvc/TrainingSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.eclipsegroup.training.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
