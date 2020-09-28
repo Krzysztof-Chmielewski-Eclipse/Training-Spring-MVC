541557295ed87633e5b953a6783b60febf01b565
Krzysztof Chmielewski <krzysztof.chmielewski@eclipsegroup.co.uk>
2020-09-29 00:16:46 +0200

    Allow choosing custom beer using URL and RequestBody

--- src/main/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerController.java
+++ src/main/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerController.java
@@ -2,15 +2,37 @@ package uk.co.eclipsegroup.training.springmvc.brewing;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("beers")
 public class BeerController {
     @GetMapping("hello")
-    public String helloBeer(Model model) {
-        model.addAttribute("beer", "Chmielu Triple STRONGEST IPA");
-        return "beer";
+    public ModelAndView helloBeer() {
+        return new ModelAndView("beer");
+    }
+
+    @GetMapping("hello/{customBeer}")
+    public ModelAndView helloBeer(@PathVariable String customBeer, Model model) {
+        populateBeer(customBeer, model);
+        return new ModelAndView("beer");
+    }
+
+    @PostMapping("hello")
+    public ModelAndView underTheCounter(@RequestParam Optional<String> customBeer, Model model) {
+        customBeer.ifPresent(beer -> populateBeer(beer, model));
+        return new ModelAndView("beer");
+    }
+
+    @ModelAttribute
+    private void beer(Model model) {
+        populateBeer("Chmielu Triple STRONGEST IPA", model);
+    }
+
+    private void populateBeer(String customBeer, Model model) {
+        model.addAttribute("beer", customBeer);
     }
 }
--- src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerControllerTest.java
+++ src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerControllerTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,12 +20,36 @@ class BeerControllerTest {
 
     @Test
     void beerView_isReturned(SoftAssertions softly) throws Exception {
-        var mvcResult = mockMvc.perform(get("/beers/hello"))
+        var mvcResult = helloBeer("/beers/hello");
+
+        softly.assertThat(extractViewName(mvcResult)).isEqualTo("beer");
+        softly.assertThat(extractAttribute(mvcResult)).contains("Chmielu");
+    }
+
+    @Test
+    void customerCanAskForCustomBeer(SoftAssertions softly) throws Exception {
+        var beerName = "Chmielu Double IPA";
+        var mvcResult = helloBeer("/beers/hello/" + beerName);
+
+        softly.assertThat(extractViewName(mvcResult)).isEqualTo("beer");
+        softly.assertThat(extractAttribute(mvcResult)).isEqualTo(beerName);
+    }
+
+    @Test
+    void customerCanAskForCustomBeer_underTheCounter(SoftAssertions softly) throws Exception {
+        var beerName = "Chmielu Quadruple IPA";
+        var mvcResult =  mockMvc.perform(post("/beers/hello").param("customBeer", beerName))
                 .andExpect(status().isOk())
                 .andReturn();
 
         softly.assertThat(extractViewName(mvcResult)).isEqualTo("beer");
-        softly.assertThat(extractAttribute(mvcResult)).contains("Chmielu");
+        softly.assertThat(extractAttribute(mvcResult)).isEqualTo(beerName);
+    }
+
+    private MvcResult helloBeer(String path) throws Exception {
+        return mockMvc.perform(get(path))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 
     private String extractAttribute(org.springframework.test.web.servlet.MvcResult mvcResult) {
