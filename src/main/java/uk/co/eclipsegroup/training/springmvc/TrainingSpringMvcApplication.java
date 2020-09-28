19e971a6b611b99290caf1e0cd0c6009374e640f
Krzysztof Chmielewski <krzysztof.chmielewski@eclipsegroup.co.uk>
2020-09-28 22:21:55 +0200

    Add controller with get mapping Add simple html page

--- src/main/java/uk/co/eclipsegroup/training/springmvc/TrainingSpringMvcApplication.java
+++ src/main/java/uk/co/eclipsegroup/training/springmvc/TrainingSpringMvcApplication.java
@@ -2,8 +2,10 @@ package uk.co.eclipsegroup.training.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class TrainingSpringMvcApplication {
 
 	public static void main(String[] args) {
