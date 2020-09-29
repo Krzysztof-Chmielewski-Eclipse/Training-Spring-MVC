6daeef94b35fce89b9fe954de7cfc42dca90fca3
Krzysztof Chmielewski <krzysztof.chmielewski@eclipsegroup.co.uk>
2020-09-29 12:49:04 +0200

    Add POST mapping for custom beer

--- src/main/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerController.java
+++ src/main/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerController.java
@@ -2,16 +2,30 @@ package uk.co.eclipsegroup.training.springmvc.brewing;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("beers")
 public class BeerController {
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String helloBeer(Model model) {
-        model.addAttribute("beer", "Chmielu STRONG IPA");
-
+    @GetMapping("hello")
+    public String helloBeer() {
         return "beer";
+    }
+
+    @PostMapping("hello")
+    public String underTheCounterBeer(@RequestParam String customBeer, Model model) {
+        model.addAttribute("customBeer", customBeer);
+        return "beer";
+    }
+
+    @GetMapping("hello/{customBeer}")
+    public String helloBeer(@PathVariable String customBeer, Model model) {
+        model.addAttribute("beer", customBeer);
+        return "beer";
+    }
+
+    @ModelAttribute
+    public void populateModel(Model model) {
+        model.addAttribute("beer", "Chmielu STRONG IPA");
     }
 }
--- src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerControllerTest.java
+++ src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/BeerControllerTest.java
@@ -1,15 +1,20 @@
 package uk.co.eclipsegroup.training.springmvc.brewing;
 
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ExtendWith(SoftAssertionsExtension.class)
 class BeerControllerTest {
     private final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new BeerController()).build();
 
@@ -20,5 +25,16 @@ class BeerControllerTest {
                 .andReturn();
 
         assertThat(mvcResult.getModelAndView().getViewName()).isEqualTo("beer");
+    }
+
+    @Test
+    void underTheCounterBeer_canBeFetched(SoftAssertions softly) throws Exception {
+        var beerName = "Chmielu Illegal IPA";
+        var mvcResult = mockMvc.perform(post("/beers/hello").param("customBeer", beerName))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        softly.assertThat(mvcResult.getModelAndView().getViewName()).isEqualTo("beer");
+        softly.assertThat(mvcResult.getModelAndView().getModel().get("customBeer")).isEqualTo(beerName);
     }
 }
