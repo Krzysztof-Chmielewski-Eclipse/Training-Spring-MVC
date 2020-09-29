5c14814fa25e2306b2c7ede157a43f3a51e5e362
Krzysztof Chmielewski <krzysztof.chmielewski@eclipsegroup.co.uk>
2020-09-29 08:46:19 +0200

    Mock external service for tests

--- src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/ControllerTest.java
+++ src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/ControllerTest.java
@@ -1,12 +1,31 @@
 package uk.co.eclipsegroup.training.springmvc.brewing;
 
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @ExtendWith(SoftAssertionsExtension.class)
-@ActiveProfiles("test")
 public class ControllerTest {
+    protected MockRestServiceServer server;
+    private ClientHttpRequestFactory originalRequestFactory;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    public void setUp() {
+        originalRequestFactory = restTemplate.getRequestFactory();
+        server = MockRestServiceServer.createServer(restTemplate);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        restTemplate.setRequestFactory(originalRequestFactory);
+    }
 }
--- src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/api/BarControllerTest.java
+++ src/test/java/uk/co/eclipsegroup/training/springmvc/brewing/api/BarControllerTest.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import uk.co.eclipsegroup.training.springmvc.brewing.ControllerTest;
@@ -13,6 +17,10 @@ import uk.co.eclipsegroup.training.springmvc.brewing.model.Beer;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.springframework.test.web.client.ExpectedCount.once;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 class BarControllerTest extends ControllerTest {
@@ -20,21 +28,35 @@ class BarControllerTest extends ControllerTest {
     private BarController barController;
     @Autowired
     private ObjectMapper objectMapper;
+    @Value("${api.url}")
+    private String apiUrl;
 
     private MockMvc mockMvc;
 
     @BeforeEach
-    void setUp() {
+    @Override
+    public void setUp() {
+        super.setUp();
         mockMvc = MockMvcBuilders.standaloneSetup(barController).build();
     }
 
     @Test
     void barIsNotEmpty() throws Exception {
+        server.expect(once(), requestTo(apiUrl + "beers"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(objectMapper.writeValueAsString(beers())));
+
         var mvcResult = mockMvc.perform(get("/bar")).andReturn();
 
         var beers = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<Beer>>() {
         });
 
         assertThat(beers).isNotEmpty().extracting(Beer::getName).noneMatch(String::isEmpty);
+    }
+
+    private List<Beer> beers() {
+        return List.of(new Beer("Chmielu IPA"), new Beer("Chmielu Strong IPA"));
     }
 }
